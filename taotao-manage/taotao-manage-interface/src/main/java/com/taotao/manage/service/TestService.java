ab48a05241db5e9c147bc8ccdcae94d4bf49c548
liaojianbin <meinitcasthavefun@foxmail.com>
2017-12-10 13:22:08 +0800

    整合后台管理系统
    1、后台管理系统服务层服务器
    2、后台管理系统表现层服务器

---
+++ taotao-manage/taotao-manage-interface/src/main/java/com/taotao/manage/service/TestService.java
@@ -0,0 +1,7 @@
+package com.taotao.manage.service;
+
+public interface TestService {
+
+	// 查询数据库中的当前时间
+	public String queryCurrentDate();
+}
---
+++ taotao-manage/taotao-manage-mapper/src/main/java/com/taotao/manage/mapper/TestMapper.java
@@ -0,0 +1,8 @@
+package com.taotao.manage.mapper;
+
+public interface TestMapper {
+	
+	//查询数据库中的当前时间
+	public String queryCurrentDate();
+
+}
